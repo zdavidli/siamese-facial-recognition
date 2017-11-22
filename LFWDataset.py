5693e39b4021c6b43b71323ae597c99922b79cae
David Li <zdavidli@gmail.com>
2017-11-22 18:51:46 -0500

    network setup, can't train

--- LFWDataset.py
+++ LFWDataset.py
@@ -1,8 +1,8 @@
 from torch.utils.data import Dataset, DataLoader
-import cv2
+from PIL import Image
 import os
-
 class LFWDataset(Dataset):
+    
     """Faces in the wild dataset."""
 
     def __init__(self, root_dir='lfw/', train=False, test=False, transform=None):
@@ -10,11 +10,15 @@ class LFWDataset(Dataset):
         self.test = test
         self.root_dir = root_dir
         self.files = []
+        self.labels = []
         self.transform = transform
         
         if (self.train and self.test) or not (self.train or self.test):
             raise ValueError('Exactly one of train and test must be set.')
         
+        """
+        Getting Train/Test splits
+        """
         dataset = set()
         if self.train:
             filename='train.txt'
@@ -24,12 +28,9 @@ class LFWDataset(Dataset):
         with open(filename) as f:
             for line in f:
                 line = line.split()
-                dataset.update({line[0], line[1]})
-                    
-        for dirpath, subdirs, walkfiles in os.walk(self.root_dir):
-            for x in walkfiles:
-                if os.path.join(dirpath.replace('lfw/', ''), x) in dataset:
-                    self.files.append(os.path.join(dirpath, x))
+                self.files.append(line[:2])
+                self.labels.append(int(line[2]))
+                
         #print(self.files)
     
     def __len__(self):
@@ -38,9 +39,13 @@ class LFWDataset(Dataset):
 #         https://stackoverflow.com/questions/16910330/return-number-of-files-in-directory-and-subdirectory
 
     def __getitem__(self, idx):
-        img_name = self.files[idx]
-        image = cv2.imread(img_name)
+        im_names = self.files[idx]
+        im1 = Image.open(os.path.join(self.root_dir, im_names[0]))
+        im2 = Image.open(os.path.join(self.root_dir, im_names[1]))
+        label = self.labels[idx]
+        
         if self.transform:
-            image = self.transform(image)
+            im1 = self.transform(im1)
+            im2 = self.transform(im2)
             
-        return image
+        return im1, im2, label
--- SiameseNet.py
+++ SiameseNet.py
@@ -3,42 +3,51 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 class SiameseNet(nn.Module):
+
     def __init__(self):
         super(SiameseNet, self).__init__()
-        self.conv1 = nn.Conv2d(3, 64, 5, stride=(1,1), padding=2)
-        self.batch1 = nn.BatchNorm2d(64)
-        self.pool1 = nn.MaxPool2d((2,2), stride=(2,2))
-        
-        self.conv2 = nn.Conv2d(64, 128, 5, stride=(1,1), padding=2)
-        self.batch2 = nn.BatchNorm2d(128)
-        self.pool2 = nn.MaxPool2d((2,2), stride=(2,2))
+        self.cnn = nn.Sequential(
+            nn.Conv2d(3, 64, 5, stride=(1,1), padding=2),
+            nn.ReLU(inplace=True),
+            nn.BatchNorm2d(64),
+            nn.MaxPool2d((2,2), stride=(2,2)),
 
-        self.conv3 = nn.Conv2d(128, 256, 3, stride=(1,1), padding=1)
-        self.batch3 = nn.BatchNorm2d(256)
-        self.pool3 = nn.MaxPool2d((2,2), stride=(2,2))
+            nn.Conv2d(64, 128, 5, stride=(1,1), padding=2),
+            nn.ReLU(inplace=True),
+            nn.BatchNorm2d(128),
+            nn.MaxPool2d((2,2), stride=(2,2)),
 
-        self.conv4 = nn.Conv2d(256, 512, 3, stride=(1,1), padding=1)
-        self.batch4 = nn.BatchNorm2d(512)
-        
-        self.linear1 = nn.Linear(512 * 16 * 16, 1024)
-        self.batch5 = nn.BatchNorm2d(1024)
-        
-        self.sigmoid = nn.Sigmoid()
+            nn.Conv2d(128, 256, 3, stride=(1,1), padding=1),
+            nn.ReLU(inplace=True),
+            nn.BatchNorm2d(256),
+            nn.MaxPool2d((2,2), stride=(2,2)),
+
+            nn.Conv2d(256, 512, 3, stride=(1,1), padding=1),
+            nn.ReLU(inplace=True),
+            nn.BatchNorm2d(512)
+        )
+
+        self.fc1 = nn.Sequential(
+            nn.Linear(512 * 16 * 16, 1024),
+            nn.ReLU(inplace=True),
+            nn.BatchNorm2d(1024)
+        )
+
+        self.fc2 = nn.Sequential(
+            nn.Linear(2*1024, 1),
+            nn.Sigmoid()
+        )
 
     def forward_once(self, x):
-        x = self.pool1(self.batch1(F.relu(self.conv1(x))))
-        x = self.pool2(self.batch2(F.relu(self.conv2(x))))
-        x = self.pool3(self.batch3(F.relu(self.conv3(x))))
-        x = self.batch4(F.relu(self.conv4(x)))
-        
-        #Flatten
-        x = x.view(-1, self.num_flat_features(x))
-        x = self.batch4(F.relu(self.linear1(x)))
-        return x
-    
+        output = self.cnn(x)
+        #flatten
+        output = output.view(output.size()[0], -1)
+        output = self.fc1(output)
+        return output
+
     def forward(self, input1, input2):
         f1 = self.forward_once(input1)
         f2 = self.forward_once(input2)
-        f12 = np.concatenate((f1, f2))
-        output = self.sigmoid(f12)
+#        print(f1, f2)
+        output = self.fc2(torch.cat((f1, f2), 1))
         return output
