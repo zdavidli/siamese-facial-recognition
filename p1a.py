930fea18509b3ece1f5f71c47fb7b03d809fbe87
David <zdavidli@gmail.com>
2017-11-24 22:51:42 +0000

    p1a without data aug done

--- p1a.py
+++ p1a.py
@@ -3,87 +3,142 @@ import torchvision
 from torch.utils.data import Dataset, DataLoader
 from torchvision import transforms as transforms
 from torch.autograd import Variable
+import torch.nn as nn
 import torch.nn.functional as F
-
+import torch.optim as optim
 
 import matplotlib.pyplot as plt
 import numpy as np
-
-import torch.nn as nn
-import torch.optim as optim
 import cv2
+import argparse
 
 from LFWDataset import LFWDataset
 from SiameseNet import SiameseNet
 
 
-def show(img, filename=None, save=False):
-    npimg = img.numpy()
-    plt.axis("off")
-    plt.imshow(np.transpose(npimg, (1,2,0)), interpolation='nearest')
-    plt.show()
-    if save and filename is not None:
-        plt.savefig(filename)
+class config():
+    train_batch_size = 64
+    train_number_epochs = 30
 
-def show_plot(iteration,loss):
+def show_plot(iteration,loss, filename='loss.png', save=False):
     plt.plot(iteration,loss)
     plt.xlabel("Iterations")
     plt.ylabel("Loss")
-    plt.savefig('output.png')
+#     plt.show()
+    if save:
+        plt.savefig(filename)
 
-def train(net):
+def train_aug(savemodel=False, model="model")
+        
+        
+def train(savemodel=False, model="model"):
+    
+    net = SiameseNet().cuda()
+    
     trainset = LFWDataset(train=True,
                       transform=transforms.Compose([transforms.Scale((128,128)),
-                                                                      transforms.ToTensor()
-                                                                      ]))
-    trainloader = DataLoader(trainset, batch_size=8, shuffle=True, num_workers=0)
+                                                      transforms.ToTensor()
+                                                      ]))
+    trainloader = DataLoader(trainset, batch_size=config.train_batch_size, shuffle=True, num_workers=0)
 
-    criterion = nn.CrossEntropyLoss()
+    criterion = nn.BCELoss()
     learning_rate = 1e-6
     optimizer = optim.Adam(net.parameters(), lr=learning_rate)
     
     counter = []
     loss_history = [] 
     iteration_number= 0
-    epochs = 10
     
-    for epoch in range(epochs):
+    for epoch in range(config.train_number_epochs):
         for i, data in enumerate(trainloader,0):
-            img0, img1 , label = data 
+            img0, img1 , label = data      
             img0, img1 , label = Variable(img0).cuda(), Variable(img1).cuda() , Variable(label).cuda()
-
             output = net(img0,img1)
-            output = torch.cat((output, 1-output), 1)
+            label = label.unsqueeze(1).float()
 
             optimizer.zero_grad()
-            loss = criterion.forward(output, label)
+            loss = criterion(output, label)
             loss.backward()
             optimizer.step()
-
-            if i % 10 == 0 :
-                print("Epoch number {}\n {}\n Current loss {}\n".format(epoch,iteration_number,loss.data[0]))
-                iteration_number += 10
+            if i %10 == 0 :
+                print("Epoch number {}\n Current loss {}\n".format(epoch,loss.data[0]))
+                iteration_number +=10
                 counter.append(iteration_number)
                 loss_history.append(loss.data[0])
-
-    show_plot(counter,loss_history)
-
-def test(net):
-    testset = LFWDataset(test=True,
-                         transform=transforms.Compose([transforms.Scale((128, 128)),
-                                                                          transforms.ToTensor()
-                                                                          ]))
-    testloader = DataLoader(testset, batch_size=8, shuffle=True, num_workers=0)
+   
+    #to see loss
+    show_plot(counter,loss_history, save=True)
+            
+    if savemodel:
+        torch.save(net.state_dict(), model)
+        print("Model saved as: " + model)
+        
+def test(loadmodel=False, model="model"):
     
+    net = SiameseNet().cuda()
+    
+    trainset = LFWDataset(train=True,
+                      transform=transforms.Compose([transforms.Scale((128,128)),
+                                                      transforms.ToTensor()
+                                                      ]))
+    trainloader = DataLoader(trainset, batch_size=config.train_batch_size, shuffle=True, num_workers=2)
+    testset = LFWDataset(test=True,
+                     transform=transforms.Compose([transforms.Scale((128, 128)),
+                                                      transforms.ToTensor()
+                                                      ]))
+    testloader = DataLoader(testset, batch_size=config.train_batch_size, shuffle=True, num_workers=2)
+    
+    if loadmodel:
+        net.load_state_dict(torch.load(model))
+        print("Loaded model: " + model)
+        
+    #Accuracy on Train Set
+    trainright=trainwrong=0.
+    for i, data in enumerate(trainloader,0):
+        img0, img1, label = data    
+        img0, img1, label = Variable(img0).cuda(), Variable(img1).cuda(), Variable(label).cuda()
+
+        output = net(img0,img1)
+        for x,y in zip(output, label):
+            if (x.data[0]<=0.5 and y.data[0]==0) or (x.data[0]>0.5 and y.data[0]==1):
+                trainright+=1
+            else:
+                trainwrong+=1
+    
+    trainacc = trainright/(trainright+trainwrong)
+    print("Accuracy on train set: {:.2f}".format(trainacc))
+    
+    #Accuracy on Test Set
+    testright=testwrong=0.
+    for i, data in enumerate(testloader,0):
+        img0, img1, label = data    
+        img0, img1, label = Variable(img0).cuda(), Variable(img1).cuda(), Variable(label).cuda()
+
+        output = net(img0,img1)
+        for x,y in zip(output, label):
+            if (x.data[0]<=0.5 and y.data[0]==0) or (x.data[0]>0.5 and y.data[0]==1):
+                testright+=1
+            else:
+                testwrong+=1
+ 
+    testacc = testright/(testright+testwrong)
+    print("Accuracy on test set: {:.2f}".format(testacc))
     
     
 def p1a():
-    net = SiameseNet().cuda()
-    train(net)
-    test(net)
-   
+    parser = argparse.ArgumentParser(description='Process loading or saving.')
+    group = parser.add_mutually_exclusive_group(required=True)
+    group.add_argument('--load', dest='load', action='store',
+                        type=str, help='File from which to load model')
+    group.add_argument('--save', dest='save', action='store',
+                        type=str, help='File to save model to')
     
-
+    args = parser.parse_args()
+    
+    if args.save:
+        train(savemodel=True, model='p1ai')
+    if args.load:
+        test(loadmodel=True, model='p1ai')
     
     
 if __name__ == "__main__":
